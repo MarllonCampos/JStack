5021fe6f5d1aced4b5551716b665895e9c34570f
Marllon Campos <marlloncampos@hotmail.com>
2021-07-20 15:36:10 -0300

    feat: learned how to work with proptypes, to type props

--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export function Post(props) {
   return (
@@ -7,9 +8,20 @@ export function Post(props) {
       <strong>{props.post.title ? props.post.title : 'Título da notícia'}</strong>
       <br />
       <small>{ props.post.subtitle ? props.post.subtitle : 'Subtítulo da notícia'}</small><br/>
-      
+      <br />
+      Media : {props.likes / 2}
     </article>
     <br />
     </>
   );
 }
+
+
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+  }).isRequired
+  
+};
