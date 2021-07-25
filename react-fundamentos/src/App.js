e43ae136939c67ac1b03c145da0aee01c3ac2161
Marllon Campos <marlloncampos@hotmail.com>
2021-07-25 00:36:21 -0300

    feat: learned how to use context and created a new context who will be responsible to pass parameter like 'dark' or 'light' according with the choose of user

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,50 +1,76 @@
 import React, { useState } from "react";
 import { Post } from "./Post";
 import { Header } from "./Header";
+import ThemeProvider from "./ThemeContext";
+
 
 const fixedPosts = [
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 20, id:Math.random() },
-  { title: 'Title#02', subtitle: 'Sub#02', likes: 10, id:Math.random() },
-  { title: 'Title#03', subtitle: 'Sub#03', likes: 50, id:Math.random() },
-  { title: 'Title#04', subtitle: 'Sub#04', likes: 50, id:Math.random() },
-]
-
+  {
+    title: "Title#01",
+    subtitle: "Sub#01",
+    likes: 20,
+    id: Math.random(),
+    read: true,
+  },
+  {
+    title: "Title#02",
+    subtitle: "Sub#02",
+    likes: 10,
+    id: Math.random(),
+    read: false,
+  },
+  {
+    title: "Title#03",
+    subtitle: "Sub#03",
+    likes: 50,
+    id: Math.random(),
+    read: false,
+  },
+  {
+    title: "Title#04",
+    subtitle: "Sub#04",
+    likes: 50,
+    id: Math.random(),
+    read: true,
+  },
+];
 
 export function App() {
-  const [posts,setPosts] = useState(fixedPosts)
+  const [posts, setPosts] = useState(fixedPosts);
   function handleRefresh() {
-    setTimeout(() => {
-    setPosts(prevState =>[...prevState, {
-       title: `Title#0${prevState.length+1}`, 
-       subtitle: `Sub#0${prevState.length+1}`, 
-       likes: parseInt(Math.random() * 100), 
-       id:Math.random(),
-    }])
-    console.log({posts});
-  },2000)
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        title: `Title#0${prevState.length + 1}`,
+        subtitle: `Sub#0${prevState.length + 1}`,
+        likes: parseInt(Math.random() * 100),
+        id: Math.random(),
+      },
+    ]);
+    console.log({ posts });
+  }
+
+  function handleRemovePost(postId) {
+    setPosts((prevState) =>
+      prevState.filter((post) => post.id !== postId)
+    );
   }
 
   return (
-    <>
-      <Header title="Notícias Diadema Blog">
+    <ThemeProvider>
+      <Header>
         <h2>Posts da semana</h2>
         <button onClick={handleRefresh}>Atualizar</button>
       </Header>
       <hr />
 
-
-      {posts.map(post =>
+      {posts.map((post) => (
         <Post
           key={post.id}
-          post={{
-              title: post.title,
-              subtitle: post.subtitle
-          }}
-          likes={post.likes}
+          onRemove={handleRemovePost}
+          post={post}
         />
-      )}
-
-
-    </>
+      ))}
+    </ThemeProvider>
   );
 }
