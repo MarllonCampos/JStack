f309856b20e83b6b72ae791562245c573c4dfa25
Marllon Campos <marlloncampos@hotmail.com>
2021-07-25 00:37:37 -0300

    feat: created a button component who can access the ThemeContext and change his styles according of the variable in Context

---
+++ react-fundamentos/src/Button.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+
+import PropTypes from "prop-types";
+import { ThemeContext } from "./ThemeContext";
+
+export default function Button(props) {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <button
+      onClick={props.onClick}
+      style={{
+        backgroundColor: theme == "dark" ? "#000" : "#fff",
+        color: theme == "dark" ? "#fff" : "#000",
+        border: theme == "light" ? "1px solid black" : "",
+        borderRadius: 5,
+        width: 80,
+        height: 35,
+        marginLeft: 15,
+      }}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
