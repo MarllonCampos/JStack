de27a918fbab070c144a0a1060d33ddcca7c3946
Marllon Campos <marlloncampos@hotmail.com>
2021-07-25 00:40:00 -0300

    feat: imported Button component who will serve as changing theme selection on Header component

--- react-fundamentos/src/Header.js
+++ react-fundamentos/src/Header.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+import { ThemeContext } from "./ThemeContext";
 
-export function Header({title, children, ...props}) {
+export function Header({ title, children, ...props }) {
+  const { onToggleTheme } = useContext(ThemeContext);
   return (
-    <>  
-       <h1 id="title">{title}</h1>
-
-       {children}
-    </>
-  )
+    <div>
+      <h1 id="title">{title}</h1>
+      <Button onClick={onToggleTheme}>Mudar Tema</Button>
+      {children}
+    </div>
+  );
 }
 
 Header.propTypes = {
   title: PropTypes.string,
-  children: PropTypes.node
-}
+  children: PropTypes.node,
+};
 
 Header.defaultProps = {
-  title: `Notícias SP Blog`
-}
+  title: `Notícias SP Blog`,
+};
