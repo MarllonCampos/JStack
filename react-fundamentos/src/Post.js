63535029a54e651b31c270911a3fb5b4046e692e
Marllon Campos <marlloncampos@hotmail.com>
2021-07-25 00:39:21 -0300

    feat: imported the PostHeader component and passed some props like post(id,title and read)

--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,15 +1,24 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import PostHeader from "./PostHeader";
 
 export function Post(props) {
   return (
     <>
-    <article>
-      <strong>{props.post.title ? props.post.title : 'Título da notícia'}</strong>
+      <article>
+      <PostHeader 
+        post={{
+          id: props.post.id,
+          title: props.post.title,
+          read: props.post.read  
+        }}
+        onRemove={props.onRemove}
+        theme={props.theme}
+      />
       <br />
       <small>{ props.post.subtitle ? props.post.subtitle : 'Subtítulo da notícia'}</small><br/>
       <br />
-      Media : {props.likes / 2}
+      Media : {props.post.likes / 2}
     </article>
     <br />
     </>
@@ -18,10 +27,13 @@ export function Post(props) {
 
 
 Post.propTypes = {
-  likes: PropTypes.number.isRequired,
   post: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired
-  }).isRequired
-  
+    subtitle: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+
+  }).isRequired,
+  onRemove: PropTypes.func.isRequired
+    
 };
