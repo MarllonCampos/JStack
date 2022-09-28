86532eb0aa22ce6c18dad6079cf350fa4e6b5119
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-09-28 14:27:33 -0300

    feat: learning the use of binding on javascript, and passing 'this' values to functions

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -10,45 +10,81 @@ import GlobalStyle from './styles/global'
 import themes from './styles/themes'
 
 export const ThemeContext = createContext();
-function App() {
-  const [localTheme, setLocalTheme] = useLocalState('theme', '');
-  const firstRender = useRef(true);
-  const [theme, setTheme] = useState(localTheme || 'dark');
+// function App() {
+//   const [localTheme, setLocalTheme] = useLocalState('theme', '');
+//   const firstRender = useRef(true);
+//   const [theme, setTheme] = useState(localTheme || 'dark');
 
 
-  const handleToogleTheme = () => {
-    setTheme((prevState) => prevState === 'dark' ? 'light' : 'dark')
+//   const handleToogleTheme = () => {
+//     setTheme((prevState) => prevState === 'dark' ? 'light' : 'dark')
+//   }
+
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark
+//   }, [theme]);
+
+
+//   useEffect(() => {
+//     if (firstRender.current) {
+//       firstRender.current = false
+//       return;
+//     }
+//     console.debug({ theme })
+//   }, [theme])
+
+//   useEffect(() => {
+//     setLocalTheme(theme)
+//   }, [theme])
+
+//   return (
+//     <ThemeProvider theme={currentTheme}>
+//       <ThemeContext.Provider
+//         value={{ theme, onToggleTheme: handleToogleTheme }}
+//       >
+//         <button onClick={handleToogleTheme}>Toggle</button>
+//         <GlobalStyle />
+//         <Layout />
+
+//       </ThemeContext.Provider>
+//     </ThemeProvider>
+//   );
+// }
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+    };
+
+    this.handleToggleTheme = this.handleToggleTheme.bind(this)
   }
 
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark
-  }, [theme]);
+  handleToggleTheme() {
+    console.log(this)
+    this.setState(prevState => ({ theme: prevState.theme === 'dark' ? 'light' : 'dark' }))
+  }
 
+  render() {
+    const { theme } = this.state
 
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false
-      return;
-    }
-    console.debug({ theme })
-  }, [theme])
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <ThemeContext.Provider
+          value={{ theme, onToggleTheme: this.handleToggleTheme }}
+        >
+          <button onClick={this.handleToggleTheme}>Toggle</button>
+          <GlobalStyle />
+          <Layout />
 
-  useEffect(() => {
-    setLocalTheme(theme)
-  }, [theme])
-
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <ThemeContext.Provider
-        value={{ theme, onToggleTheme: handleToogleTheme }}
-      >
-        <button onClick={handleToogleTheme}>Toggle</button>
-        <GlobalStyle />
-        <Layout />
-
-      </ThemeContext.Provider>
-    </ThemeProvider>
-  );
+        </ThemeContext.Provider>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default App;
