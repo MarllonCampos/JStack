0b0e14d36c2ce7df5c02cf94bba43f6a034c12cd
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-09-29 14:48:16 -0300

    feat: retrieve route params and query params using class components

--- react-fundamentos/src/pages/Post/index.js
+++ react-fundamentos/src/pages/Post/index.js
@@ -1,13 +1,33 @@
 import React, { useMemo } from 'react'
 import { useParams, useLocation } from 'react-router-dom'
-export default function Post() {
-  const params = useParams();
-  const { search } = useLocation();
-  const queryParams = useMemo(() => new URLSearchParams(search), [search])
+
+// export default function Post() {
+//   const params = useParams();
+//   const { search } = useLocation();
+//   const queryParams = useMemo(() => new URLSearchParams(search), [search])
 
 
-  console.log(params, queryParams.get('queryParam'));
-  return (
-    <h1>Post</h1>
-  )
+//   console.log(params, queryParams.get('queryParam'));
+//   return (
+//     <h1>Post</h1>
+//   )
+// }
+
+
+export default class Post extends React.Component {
+  constructor(props) {
+    super(props)
+
+    const { search } = this.props.location;
+
+    this.queryParams = new URLSearchParams(search)
+  }
+
+  render() {
+
+    console.log(this.props.match.params, this.queryParams.get('queryParam'));
+    return (
+      <h1>Post</h1>
+    )
+  }
 }
