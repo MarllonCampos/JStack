00e626b5ef925217661ddee342ff7a67ac946c43
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-10-06 17:40:57 -0300

    feat: create button component

--- myContacts/web/src/Pages/NewContact/index.js
+++ myContacts/web/src/Pages/NewContact/index.js
@@ -1,5 +1,6 @@
 // import { Container } from './styles';
 
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -18,6 +19,12 @@ function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
